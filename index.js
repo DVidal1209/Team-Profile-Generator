bcb9abb8f148acee494006fe864cbfba7d76f197
Daniel Vidal <daniel.vidal17@yahoo.com>
2022-10-26 16:47:45 -0400

    Added comments

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+// Importing other libraries and declariing variabled
 const Manager = require("./lib/manager");
 const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
 const fs = require("fs");
 const inquirer = require('inquirer');
+
+// Initial declaration of html data
 var html = `<!DOCTYPE html>
 <html lang="en">
 <head>
@@ -24,6 +27,7 @@ var html = `<!DOCTYPE html>
 
 
 inquirer
+    // Collection of Manager Information
     .prompt([
         {
             type: "input",
@@ -52,10 +56,11 @@ inquirer
         selection()
     })
     
-    
+    // Function to accept input in the command line
 async function selection(){
     inquirer.prompt([
         {
+            // Question to determine what the user wishes to do
             type: "list",
             message: "What would you like to do?",
             name: "selection",
@@ -63,6 +68,7 @@ async function selection(){
         }
     ])
     .then((response) => {
+        // Questions for when user wisher to add an Engineer
         if (response.selection === "Add an Engineer"){
             inquirer.prompt([
                 {
@@ -87,11 +93,14 @@ async function selection(){
                 }
             ])
             .then((response) => {
+                // Appending Engineer card to html element
                 const engineer = new Engineer(response.enName, response.enId, response.enEmail, response.enGitHub);
                 html = engineer.appendHtml(html);
                 selection();
             })
-        } else if (response.selection === "Add an Intern"){
+        } else 
+        // Questions for when user wisher to add an Intern
+        if (response.selection === "Add an Intern"){
                 inquirer.prompt([
                     {
                         type: "input",
@@ -115,11 +124,13 @@ async function selection(){
                     }
                 ])
                 .then((response) => {
+                    // Appending Intern card to html element
                     const intern = new Intern(response.inName, response.inId, response.inEmail, response.inSchool);
                     html = intern.appendHtml(html);
                     selection()
                 })
             } else {
+                // Adding the last part of the html file then creating the file
                 html += `
             </div>
         </div>
